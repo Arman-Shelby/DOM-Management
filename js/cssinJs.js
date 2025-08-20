93559395a9d9b73422a305dbb26fc1c454c497b3
Arman Mir <mirarman8583@gmail.com>
2025-08-20 07:11:47 +0600

    dynamic styles

---
+++ js/cssinJs.js
@@ -0,0 +1,21 @@
+// console.log('file connected');
+
+// const sections = document.getElementsByTagName('section');
+const sections = document.querySelectorAll('section')
+// console.log(sections);
+
+for (section of sections){
+    section.style.backgroundColor = 'lightblue';
+    section.style.border= '3px solid green';
+    section.style.margin = '10px';
+    section.style.borderRadius = '20px';
+    section.style.padding = '20px';
+    console.log(section);
+
+}
+
+//dynamic class
+// for(const section of sections){
+//     section.classList.add('sectionCard');
+// }
+
